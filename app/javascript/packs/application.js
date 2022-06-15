ba3bad00b375c3cec95bf9d04a9be052133b2356
Raúl Cisternas <r.cisternas.w@gmail.com>
2022-06-15 14:56:59 -0400

    ADD: Methods in application js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,17 @@ Turbolinks.start()
 ActiveStorage.start()
 
 require('bootstrap')
+
+$(document).on('click', 'form .add_fields', function(event) {
+  var regexp, time;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+  $(this).closest('.dynamic-data').find($(this).data('response')).append($(this).data('fields').replace(regexp, time));
+  return event.preventDefault();
+});
+
+$(document).on('click', 'form .remove_fields', function(event) {
+  $(this).prev('.destroy_elem').val('true');
+  $(this).closest('fieldset').hide();
+  return event.preventDefault();
+});
