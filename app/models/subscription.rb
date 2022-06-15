e0ee71f3f81e8c2c1f7514f9bd2f963a0f84cd04
Raúl Cisternas <r.cisternas.w@gmail.com>
2022-06-15 13:44:54 -0400

    UPDATE: Relationships in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,12 @@
 class Subscription < ApplicationRecord
-  has_and_belongs_to_many :products
+  has_many :orders, dependent: :destroy
+  has_many :subscriptions_products, dependent: :destroy
+  has_many :products, through: :subscriptions_products
+  accepts_nested_attributes_for :subscriptions_products, allow_destroy: true
+  after_save :calculate_total_amount
+
+  def calculate_total_amount
+    total_amount = products.sum(:price)
+    update_column(:total_amount, total_amount)
+  end
 end
