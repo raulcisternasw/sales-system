42560da679e445450c2e58ec0a587d864621d0f6
Raúl Cisternas <r.cisternas.w@gmail.com>
2022-06-16 00:23:52 -0400

    ADD: Route for webpay status

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :orders
   # Webpay Routes
   get '/webpayplus/return_url', to: 'orders#webpay_commit', as: :webpay_commit
+  get 'webpayplus/status/:token', to: 'orders#webpay_status', as: :webpay_status
 end
