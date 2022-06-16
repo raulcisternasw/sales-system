4dc070fea468320d2cb8c467e466e9b24fd161b3
Raúl Cisternas <r.cisternas.w@gmail.com>
2022-06-16 00:08:55 -0400

    ADD: After create generate buy order in model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,10 @@ class Order < ApplicationRecord
   enum payment_type_code: [:debit_sale, :normal_sale, :sale_installments, :three_installments_without_interest,
                            :two_installments_without_interest, :n_installments_without_interest, :prepaid_sale]
   validates_uniqueness_of :user_id, conditions: -> { where(status: :authorized) }, if: Proc.new { |order| order.authorized? }, message: 'already has an order created.'
+  after_create :generate_buy_order
+
+  # Method to generate buy order
+  def generate_buy_order
+    self.buy_order = "sales-system-#{ self.id }"
+  end
 end
