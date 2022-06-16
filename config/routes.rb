8b08bc6332dc7a4af4d530ee60aa65046ace9920
Raúl Cisternas <r.cisternas.w@gmail.com>
2022-06-16 00:04:04 -0400

    ADD: Comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Devise Routes
   devise_for :users
+  # Users Routes
   resources :users
+  # Subscriptions Routes
   resources :subscriptions
+  # Products Routes
   resources :products
+  # Orders Routes
   resources :orders
 end
